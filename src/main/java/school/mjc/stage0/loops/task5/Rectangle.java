49b2302610ceecf2178321776ed153d7eba22829
VitaliyBaranovskyy <40470956+VitaliyBaranovskyy@users.noreply.github.com>
2023-04-10 13:44:49 +0300

    Update Rectangle.java

--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -2,6 +2,20 @@ package school.mjc.stage0.loops.task5;
 
 public class Rectangle {
     public void printRectangleFrom8s(int length, int height){
-
+        int i;
+        int j;
+        for (i =1; i <= height; i++){
+            for (j=1; j<=length; j++){
+             if (i==1 || i == height || j==1 || j== length)
+                 System.out.print("8 ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main (String [] args){
+     Rectangle main = new Rectangle();
+        main.printRectangleFrom8s(11,7);
     }
 }

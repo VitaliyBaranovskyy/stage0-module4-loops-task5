81ff4c7c9334174b4ec908f9a5bd55afd6dbf455
VitaliyBaranovskyy <40470956+VitaliyBaranovskyy@users.noreply.github.com>
2023-04-10 13:22:31 +0300

    Update Cross.java

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,20 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
-
+        int i;
+        int j;
+        for (i=1; i<=sideLength; i++){
+             for (j=1; j<=sideLength; j++){
+              if (i==sideLength/2+1 || j==sideLength/2+1)
+                  System.out.print("8 ");
+                 else
+                     System.out.print("  ");
+             }
+            System.out.println();
+        }
     }
-}
+    public static void main (String [] args ){
+       Cross main = new Cross();
+        main.printCross(7);
+    }
+   }
